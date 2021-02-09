a36d86276e597b3a818145d4aff11fc2b2bf4812
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-02-09 11:28:26 +0700

    chen

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, jsonify
+from pyFTS.data import Enrollments
+from pyFTS.partitioners import Grid
+from pyFTS.models import chen, cheng
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def hello_world():
-    return jsonify({'message': 'Hello World'})
+def index():
+    train = [Enrollments.get_data()]
+    test = [Enrollments.get_data()]
+    fs = Grid.GridPartitioner(data=train, npart=10)
+    model = chen.ConventionalFTS(partitioner=fs)
+    model.fit(train)
+    forecasts = model.predict(test)
+
+    data = {
+        'train': train,
+        'test': test,
+        'forecast': forecasts,
+        # 'rmse': er
+    }
+
+    # return jsonify({'message': 'Hello World'})
+    return jsonify(data)
 
 app.run(debug=True)
